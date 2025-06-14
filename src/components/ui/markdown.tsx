51b84117416b078182d18540ed89a32cf482f4d4
noriapi <70106808+noriapi@users.noreply.github.com>
2025-06-15 06:36:47 +0900

    Markdownスタイルを修正

--- src/components/ui/markdown.tsx
+++ src/components/ui/markdown.tsx
@@ -7,15 +7,25 @@ import type { ComponentProps } from "solid-js";
 export const A = Link;
 
 export function H1(props: HeadingProps) {
-  return <Heading {...props} size="4xl" my="3" />;
+  return <Heading {...props} size="3xl" my="3" />;
 }
 
 export function H2(props: HeadingProps) {
-  return <Heading {...props} size="lg" mt="7" mb="2" />;
+  return (
+    <Heading
+      {...props}
+      size="2xl"
+      mt="14"
+      mb="2"
+      borderBottomWidth="1px"
+      borderColor="border.default"
+      pb="2"
+    />
+  );
 }
 
 export function P(props: TextProps) {
-  return <Text {...props} my="1.5" />;
+  return <Text {...props} my="1rem" />;
 }
 
 export function Ul(props: ComponentProps<typeof styled.ul>) {
@@ -24,10 +34,15 @@ export function Ul(props: ComponentProps<typeof styled.ul>) {
   );
 }
 
+export function Li(props: ComponentProps<typeof styled.li>) {
+  return <styled.li {...props} my="1" />;
+}
+
 export const components = {
   a: A,
   h1: H1,
   h2: H2,
   p: P,
   ul: Ul,
+  li: Li,
 };
