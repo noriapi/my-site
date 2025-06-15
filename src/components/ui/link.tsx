b2a513bc6e2d1f42cae52bd7c0b8b63b35e9af20
noriapi <70106808+noriapi@users.noreply.github.com>
2025-06-15 09:52:07 +0900

    外部リンクにはアイコンがつくように変更

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,1 +1,29 @@
-export { Link, type LinkProps } from "./styled/link";
+import {
+  Link as StyledLink,
+  type LinkProps as StyledLinkProps,
+} from "./styled/link";
+import ExternalLinkIcon from "~icons/lucide/external-link";
+import { icon, type IconVariantProps } from "styled-system/recipes";
+import { Show, splitProps } from "solid-js";
+import isAbsoluteUrl from "is-absolute-url";
+
+export interface LinkProps extends StyledLinkProps {
+  icon?: IconVariantProps;
+  external?: boolean;
+}
+
+export function Link(props: LinkProps) {
+  const [, omitChildren] = splitProps(props, ["children"]);
+
+  const isExternal = () =>
+    props.external || (props.href != null && isAbsoluteUrl(props.href));
+
+  return (
+    <StyledLink {...omitChildren}>
+      {props.children}
+      <Show when={isExternal()}>
+        <ExternalLinkIcon class={icon(props.icon)} />
+      </Show>
+    </StyledLink>
+  );
+}
