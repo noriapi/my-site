e081e6f2dcaa58bcab28b0a334435412e14fe5b6
noriapi <70106808+noriapi@users.noreply.github.com>
2025-06-26 16:56:17 +0900

    すべての画像が読み込まれるまで待機するように修正

--- tests/visual.test.ts
+++ tests/visual.test.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { expect, test, type Page } from "@playwright/test";
 import path from "node:path";
 
 test.describe("Take screenshots", () => {
@@ -10,6 +10,8 @@ test.describe("Take screenshots", () => {
   ].forEach((targetPage) => {
     test(targetPage.name, async ({ page }, info) => {
       await page.goto(targetPage.path, { waitUntil: "networkidle" });
+      await seeFullPage(page);
+      await waitForAllImages(page);
 
       const ss = path.normalize(
         path.format({
@@ -23,3 +25,30 @@ test.describe("Take screenshots", () => {
     });
   });
 });
+
+async function seeFullPage(page: Page) {
+  await page.evaluate(async () => {
+    // scroll to bottom
+    for (let i = 0; i < document.body.scrollHeight; i += 100) {
+      window.scrollTo(0, i);
+      await delay(50);
+    }
+
+    // scroll to top
+    for (let i = document.body.scrollHeight; i > 0; i -= 100) {
+      window.scrollTo(0, i);
+      await delay(50);
+    }
+  });
+}
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function waitForAllImages(page: Page) {
+  for (const img of await page.getByRole("img").all()) {
+    await expect(img).toHaveJSProperty("complete", true);
+    await expect(img).not.toHaveJSProperty("naturalWidth", 0);
+  }
+}
